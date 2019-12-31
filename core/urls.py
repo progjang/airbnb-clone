04e357141d73f7294d24b00f4c3b808342184c4a
David <wooritobi@gmail.com>
2020-01-01 00:32:20 +0900

    #10 Introduction to Views and Urls (include templates) completed

--- core/urls.py
+++ core/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from core import views
+app_name = "core_app"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("core.urls", namespace="core"))
+    path("", views.all_rooms, name="home")
 ]
 
 if settings.DEBUG:
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from datetime import datetime
+from rooms import models as room_models
 # Create your views here.
+
+
+def all_rooms(request):
+    rooms = room_models.Room.objects.all()
+    return render(request, "core/home.html", context={
+        "rooms": rooms
+    })
